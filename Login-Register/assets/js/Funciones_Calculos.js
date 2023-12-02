422f7e7782a7bcd837f0073387f9c7276bd388e1
bmcorsof <90282494+bmcorsof@users.noreply.github.com>
2023-12-02 16:29:43 -0400

    eliminar carpetas

--- Login-Register/assets/js/Funciones_Calculos.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('pareto').addEventListener('click', function() {
-    var companyId = this.getAttribute('data-id');
-    window.location.href = 'Calculo_Pareto.php?id=' + companyId;
-});
-
-document.getElementById('profitability').addEventListener('click', function() {
-    var companyId = this.getAttribute('data-id');
-    window.location.href = 'Calculo_Rentabilidad.php?id=' + companyId;
-});
